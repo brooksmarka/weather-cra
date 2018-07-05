8318d2943de19469125f9871863502525a6634a0
Mark Brooks <Markabrooks@gmail.com>
2018-07-05 14:54:52 -0600

    error with Line chart displaying

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Weather app built using React, Redux based on Stephen Grider's Udemy React Course
 
-![sample](https://www.useloom.com/share/5e9f3ba4350043fe9c93e679276b1518)
+[https://www.useloom.com/share/5e9f3ba4350043fe9c93e679276b1518]
 This project was bootstrapped with [Create React App](https://github.com/facebookincubator/create-react-app).
 
 You can find the most recent version of this guide [here](https://github.com/facebookincubator/create-react-app/blob/master/packages/react-scripts/template/README.md).
--- src/components/weather_list.js
+++ src/components/weather_list.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
-//import { fetchWeather } from "../actions";
+import { LineChart, Line } from "recharts";
 
 class WeatherList extends Component {
   static propTypes = { weather: PropTypes.array };
@@ -11,9 +11,18 @@ class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
 
+    const temps = cityData.list.map(listData => listData.main.temp);
+
+    console.log(temps);
+
     return (
       <tr key={name}>
         <td>{name}</td>
+        <td>
+          <LineChart width={120} height={180} data={temps}>
+            <Line type="monotone" stroke="#8884d8" />
+          </LineChart>
+        </td>
       </tr>
     );
   }
